53189bd22175fbaf91d695a1fc1a7cdff0424a28
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-08-26 01:27:13 +0100

    feat(cmdrc): use ^$ rather than $T to to batch commands together

--- cmdrc.bat
+++ cmdrc.bat
@@ -6,16 +6,40 @@
 ::
 ::
 
+doskey norg=gron --ungron
+doskey l=lazygit $*
+doskey r=trash $*
+doskey b=bat --style=plain --theme ansi $*
 doskey g=git $*
-doskey c=cls
 doskey n=nvim -u %USERPROFILE%\dotfiles\neovim.lua $*
+doskey ?=tldr --static $*
+doskey dns=nslookup $*
+doskey s=s -p google $*
 
 ::
 :: ZOXIDE ALIASES
 ::
 
 doskey z=zoxide query $*
-doskey t=zoxide add $1 $T cd /d $1 $T eza --icons --git-ignore --sort=changed --across --classify
+doskey t=zoxide add $1 ^& cd /d $1 ^& eza --icons --git-ignore --sort=changed --across --classify
+
+::
+:: FAST TRAVEL ALIASES
+::
+
+doskey .=cd /d .. ^& eza --icons --git-ignore --sort=changed --across --classify
+doskey ..=cd /d ..\.. ^& eza --icons --git-ignore --sort=changed --across --classify
+doskey ...=cd /d ..\..\.. ^& eza --icons --git-ignore --sort=changed --across --classify
+doskey ....=cd /d ..\..\..\.. ^& eza --icons --git-ignore --sort=changed --across --classify
+doskey .....=cd /d ..\..\..\..\.. ^& eza --icons --git-ignore --sort=changed --across --classify
+
+::
+:: UNIX ALIASES
+::
+
+doskey cp=robocopy $*
+doskey mv=move $*
+doskey pwd=cd
 
 ::
 :: EZA ALIASES
