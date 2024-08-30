df603d32cb324eaf774fb0471d0b37e0247485ff
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-08-30 10:46:14 +0100

    feat(wezterm): remove starship config env variable

--- wezterm.lua
+++ wezterm.lua
@@ -437,7 +437,6 @@ config.set_environment_variables = {
 	CLINK_PATH = DOTFILES .. "\\scripts",
 	CLINK_SETTINGS = DOTFILES,
 	CLINK_INPUTRC = DOTFILES,
-	STARSHIP_CONFIG = DOTFILES .. "\\starship.toml",
 	GIT_CONFIG_GLOBAL = DOTFILES .. "\\git.ini",
 	LG_CONFIG_FILE = DOTFILES .. "\\lazygit.yml",
 	FZF_DEFAULT_OPTS = "--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 --color=selected-bg:#45475a --multi",
