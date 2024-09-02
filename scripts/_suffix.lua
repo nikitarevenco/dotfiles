f01e0e527f7a569714a0edaa3c694194fee23bd8
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-09-02 17:40:31 +0100

    feat(scripts): add suffix script

--- scripts/_suffix.lua
+++ scripts/_suffix.lua
@@ -1,62 +1,59 @@
-local suffix_aliases = {
-	[".git"] = "git clone",
-}
+settings.add("color.suffix", "", "Color for when suffix is matched")
+settings.add("suffix.aliases", ".git=git clone;", "Suffix aliases")
 
--- For highlighting
-local function file_exists_in_directory(directory, filename)
-	local separator = "\\"
-	local path = directory .. separator .. filename
-	local file = io.open(path, "r")
-	if file then
-		file:close()
-		return true
+local suffixes = {}
+
+for suffix_prepend_pair in string.gmatch(settings.get("suffix.aliases"), "([^;]+)") do
+	local suffix = {}
+	for suffix_or_prepend in string.gmatch(suffix_prepend_pair, "([^=]+)") do
+		table.insert(suffix, suffix_or_prepend)
+	end
+
+	suffixes[suffix[1]] = suffix[2]
+end
+
+local function escape_pattern(str)
+	return str:gsub("(%W)", "%%%1")
+end
+
+local function get_suffix(word)
+	for suffix in pairs(suffixes) do
+		local pattern = escape_pattern(suffix) .. "$"
+		if word:match(pattern) then
+			return suffix
+		end
 	end
 	return false
 end
 
-local function is_in_path(program)
-	local path_env = os.getenv("PATH")
-	local path_ext = os.getenv("PATHEXT")
+local suffix_classifier = clink.classifier(1)
+function suffix_classifier:classify(commands)
+	local line = commands[1].line_state:getline()
+	local color = settings.get("color.suffix") or ""
+	local classifications = commands[1].classifications
+	local last_index = 1
 
-	if not path_ext then
-		error("%PATHEXT% is not available")
-	elseif not path_env then
-		error("%PATH% is not available")
-	end
+	for word in line:gmatch("%S+") do
+		local match = get_suffix(word)
+		local start_index, end_index = string.find(line, word, last_index, true)
+		last_index = end_index + 1
 
-	local extensions = {}
-
-	for str in string.gmatch(path_ext, "([^;]+)") do
-		table.insert(extensions, str)
-	end
-
-	local split_path = string.gmatch(path_env, "[^;]+")
-
-	for dir in split_path do
-		for _, ext in ipairs(extensions) do
-			if file_exists_in_directory(dir, program .. ext) then
-				return true
-			end
+		if match and suffixes[match] then
+			classifications:applycolor(start_index, end_index - start_index + 1, color)
 		end
 	end
-
-	return false
 end
 
-local function string_endswith(string, suffix)
-	return string:sub(-#suffix) == suffix
-end
-
-local function onfilterinput(text)
-	for suffix, command in pairs(suffix_aliases) do
-		if string_endswith(text, suffix) then
-			clink.argmatcher(text)
-			local text_without_suffix = string.sub(text, 0, #text - #suffix)
-			if not is_in_path(text_without_suffix) then
-				return command .. " " .. text
-			end
+local function onfilterinput(line)
+	local words = {}
+	for word in line:gmatch("%S+") do
+		local match = get_suffix(word)
+		if match and suffixes[match] then
+			word = suffixes[match] .. " " .. word
 		end
+		table.insert(words, word)
 	end
+	return table.concat(words, " ")
 end
 
 if clink.onfilterinput then
