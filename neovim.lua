56bf31c3abff6c494b378e820a421c6a2aa817f2
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-09-14 01:23:37 +0100

    feat(nvim): remove which-key

--- neovim.lua
+++ neovim.lua
@@ -155,24 +155,6 @@ require("lazy").setup({
 		opts = {},
 	},
 	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		},
-		keys = {
-			{
-				"<leader>?",
-				function()
-					require("which-key").show({ global = false })
-				end,
-				desc = "Buffer Local Keymaps (which-key)",
-			},
-		},
-	},
-	{
 		"akinsho/toggleterm.nvim",
 		opts = {
 			open_mapping = "<leader>e",
