67e03b4ceacabf8c534e621ef451444ee9a853de
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-09-14 01:22:50 +0100

    feat(nvim): remove unneeded plugins and unneeded keymaps

--- neovim.lua
+++ neovim.lua
@@ -1,11 +1,3 @@
-local function keybind(description, shortcut, action, modes, opts)
-	local new_modes = modes or "n"
-	local new_opts = opts or {}
-	new_opts.desc = description
-
-	vim.keymap.set(new_modes, shortcut, action, new_opts)
-end
-
 local function toggle_conceal()
 	if vim.wo.conceallevel == 2 then
 		vim.opt.conceallevel = 0
@@ -62,12 +54,16 @@ vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, si
 vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true, desc = "move up" })
 vim.keymap.set("v", "<", "<gv", { desc = "indent" })
 vim.keymap.set("v", ">", ">gv", { desc = "dedent" })
-vim.keymap.set("n", "<leader>z", "<cmd>lua vim.wo.number = not vim.wo.number<cr>", { desc = "toggle left panel" })
+vim.keymap.set(
+	"n",
+	"<leader>z",
+	"<cmd>lua vim.wo.number = not vim.wo.number;vim.wo.relativenumber = not vim.wo.relativenumber<cr>",
+	{ desc = "toggle line numbers" }
+)
 vim.keymap.set("i", "<c-z>", "<C-o>", { desc = "execute normal command" })
 vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "escape and clear hlsearch" })
 vim.keymap.set("n", "<bs>", "<cmd>edit #<cr>", { silent = true, desc = "alternate buffer" })
 vim.keymap.set("n", "<leader>b", toggle_conceal, { desc = "toggle conceal" })
-vim.keymap.set("n", "<c-f>", "<cmd>w<cr>", { desc = "format" })
 vim.keymap.set("t", "<esc>", "<cmd>ToggleTerm<cr>", { desc = "exit terminal mode" })
 vim.keymap.set("i", "<C-d>", "<C-o><cmd>d<CR>", { desc = "delete line" })
 vim.keymap.set("i", "<C-v>", "<esc>pa", { desc = "paste" })
@@ -76,6 +72,8 @@ vim.keymap.set("n", "go", "<cmd>call append(line('.'), repeat([''], v:count1))<C
 vim.keymap.set("x", "g/", "<esc>/\\%V", { silent = false, desc = "search inside visual selection" })
 vim.keymap.set("n", "<right>", "<C-w>", { desc = "window commands" })
 vim.keymap.set("n", "J", "mzJ`z", { desc = "join line but keep cursor position" })
+vim.keymap.set("n", "<c-u>", "<c-u>zz")
+vim.keymap.set("n", "<c-d>", "<c-d>zz")
 
 vim.api.nvim_create_autocmd(
 	{ "FocusLost", "ModeChanged", "TextChanged", "BufEnter" },
@@ -124,18 +122,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
--- local instant_lsp_path = vim.fn.stdpath("data") .. "/instant-lsp.nvim"
--- if not vim.loop.fs_stat(instant_lsp_path) then
--- 	vim.fn.system({
--- 		"git",
--- 		"clone",
--- 		"--filter=blob:none",
--- 		"https://github.com/nikitarevenco/instant-lsp.nvim",
--- 		instant_lsp_path,
--- 	})
--- end
--- vim.opt.rtp:prepend(instant_lsp_path)
-
 vim.opt.rtp:prepend("C:\\Users\\micro\\repos\\instant-lsp.nvim")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -153,7 +139,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	require("instant-lsp").setup({
-		languages = { "lua" },
+		languages = { "lua", "markdown" },
 		disable_feature = {
 			snippets = true,
 			ts_context = true,
@@ -161,146 +147,42 @@ require("lazy").setup({
 		},
 	}),
 	{
-		"nikitarevenco/nvim-treesitter-textobjects",
-		branch = "personal",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-		opts = {
-			textobjects = {
-				move = {
-					enable = true,
-					goto_next_start = {
-						["]a"] = "@parameter.inner",
-						["]l"] = "@loop.outer",
-						["]m"] = "@function.outer",
-						["]i"] = "@conditional.outer",
-						["]k"] = "@class.outer",
-						["]f"] = "@call.outer",
-						["]c"] = "@block.outer",
-						["]T"] = "@assignment.outer",
-						["]h"] = "@return.outer",
-						["]o"] = "@annotation.outer",
-						["]O"] = "@annotation.outer",
-					},
-					goto_previous_start = {
-						["[a"] = "@parameter.inner",
-						["[l"] = "@loop.outer",
-						["[m"] = "@function.outer",
-						["[i"] = "@conditional.outer",
-						["[k"] = "@class.outer",
-						["[f"] = "@call.outer",
-						["[c"] = "@block.outer",
-						["[T"] = "@assignment.outer",
-						["[h"] = "@return.outer",
-						["[o"] = "@annotation.outer",
-						["[O"] = "@annotation.outer",
-					},
-				},
-				select = {
-					enable = true,
-					lookahead = true,
-					keymaps = {
-						["T"] = { query = "@assignment.outer", desc = "assignment" },
-						["O"] = { query = "@statement.outer", desc = "statement" },
-						["io"] = { query = "@annotation.inner", desc = "type annotation" },
-						["ao"] = { query = "@annotation.outer", desc = "type annotation" },
-						["ih"] = { query = "@return.inner", desc = "return" },
-						["ah"] = { query = "@return.outer", desc = "return" },
-						["as"] = { query = "@assignment.lhs", desc = "assignment lhs" },
-						["is"] = { query = "@assignment.rhs", desc = "assignment rhs" },
-						["ac"] = { query = "@block.outer", desc = "block" },
-						["ic"] = { query = "@block.inner", desc = "block" },
-						["af"] = { query = "@call.outer", desc = "function call" },
-						["if"] = { query = "@call.inner", desc = "function call" },
-						["ak"] = { query = "@class.outer", desc = "class" },
-						["ik"] = { query = "@class.inner", desc = "class" },
-						["ai"] = { query = "@conditional.outer", desc = "conditional" },
-						["ii"] = { query = "@conditional.inner", desc = "conditional" },
-						["am"] = { query = "@function.outer", desc = "function" },
-						["im"] = { query = "@function.inner", desc = "function" },
-						["al"] = { query = "@loop.outer", desc = "loop" },
-						["il"] = { query = "@loop.inner", desc = "loop" },
-						["N"] = { query = "@number.inner", desc = "number" },
-						["aa"] = { query = "@parameter.outer", desc = "argument" },
-						["ia"] = { query = "@parameter.inner", desc = "argument" },
-					},
-				},
-			},
-		},
-		config = function(_, opts)
-			local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
-
-			vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
-			vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
-			vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
-			vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
-			vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
-			vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
-			require("nvim-treesitter.configs").setup(opts)
-		end,
-	},
-	{
-		"chrisgrieser/nvim-various-textobjs",
-		config = function()
-			vim.keymap.set("n", "gx", function()
-				require("various-textobjs").url()
-
-				local foundURL = vim.fn.mode():find("v")
-				if not foundURL then
-					return
-				end
-
-				vim.cmd.normal({ '"zy', bang = true })
-				local url = vim.fn.getreg("z")
-				vim.ui.open(url)
-			end, { desc = "URL Opener" })
-
-			require("various-textobjs").setup({
-				useDefaultKeymaps = false,
-			})
-
-			vim.keymap.set({ "o", "x" }, "ir", "<cmd>lua require('various-textobjs').subword('inner')<cr>")
-			vim.keymap.set({ "o", "x" }, "ar", "<cmd>lua require('various-textobjs').subword('outer')<cr>")
-			vim.keymap.set({ "o", "x" }, "A", "<cmd>lua require('various-textobjs').entireBuffer()<cr>")
-			vim.keymap.set({ "o", "x" }, "o", "<cmd>lua require('various-textobjs').nearEoL()<cr>")
-			vim.keymap.set({ "o", "x" }, "n", "<cmd>lua require('various-textobjs').lineCharacterwise('inner')<cr>")
-			vim.keymap.set({ "o", "x" }, "iv", "<cmd>lua require('various-textobjs').value('inner')<cr>")
-			vim.keymap.set({ "o", "x" }, "av", "<cmd>lua require('various-textobjs').value('outer')<cr>")
-			vim.keymap.set({ "o", "x" }, "ik", "<cmd>lua require('various-textobjs').key('inner')<cr>")
-			vim.keymap.set({ "o", "x" }, "ak", "<cmd>lua require('various-textobjs').key('outer')<cr>")
-			vim.keymap.set({ "o", "x" }, "U", "<cmd>lua require('various-textobjs').url()<cr>", { desc = "url" })
-			vim.keymap.set({ "o", "x" }, "ie", "<cmd>lua require('various-textobjs').chainMember('inner')<cr>")
-			vim.keymap.set({ "o", "x" }, "ae", "<cmd>lua require('various-textobjs').chainMember('outer')<cr>")
-		end,
-	},
-	{
 		"MagicDuck/grug-far.nvim",
 		cmd = "GrugFar",
+		keys = {
+			{ "<leader>s", "<cmd>GrugFar<cr>", desc = "search and replace" },
+		},
 		opts = {},
 	},
 	{
-		"stevearc/oil.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		lazy = false,
+		"folke/which-key.nvim",
+		event = "VeryLazy",
 		opts = {
-			delete_to_trash = true,
-			skip_confirm_for_simple_edits = true,
-			keymaps = {
-				["<left>"] = "actions.parent",
-				["<right>"] = "actions.select",
-				["<esc>"] = "actions.close",
-				["<leader>"] = { callback = ":w" },
-			},
-			view_options = {
-				is_always_hidden = function(name)
-					return name == ".."
-				end,
-				sort = {
-					{ "mtime", "desc" },
-				},
-			},
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
 		},
 		keys = {
-			{ "<leader>n", "<cmd>Oil<cr>", desc = "file manager" },
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
+	{
+		"akinsho/toggleterm.nvim",
+		opts = {
+			open_mapping = "<leader>e",
+			insert_mappings = false,
+			terminal_mappings = false,
+			autochdir = true,
+			shade_terminals = false,
+			direction = "tab",
+			shell = vim.fn.has("win32") and 'cmd.exe /s /k "clink inject -q && %userprofile%\\dotfiles\\doskeys.cmd"'
+				or "zsh",
 		},
 	},
 	{
@@ -308,12 +190,22 @@ require("lazy").setup({
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"sindrets/diffview.nvim",
-			"nvim-telescope/telescope.nvim",
 			"ibhagwan/fzf-lua",
 		},
 		opts = {
 			disable_hint = true,
 			graph_style = "unicode",
+			mappings = {
+				commit_editor = {
+					["<esc>"] = "Close",
+				},
+				status = {
+					["<esc>"] = "Close",
+				},
+				rebase_editor = {
+					["<esc>"] = "Close",
+				},
+			},
 		},
 		keys = {
 			{
@@ -321,6 +213,133 @@ require("lazy").setup({
 				"<cmd>Neogit<CR>",
 				mode = "n",
 				desc = "neogit",
+			},
+		},
+	},
+	{
+		"ibhagwan/fzf-lua",
+		keys = {
+			{ "<leader>y", "<cmd>FzfLua grep_project<cr>", desc = "grep" },
+		},
+	},
+	{
+		"stevearc/oil.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		lazy = false,
+		opts = function()
+			function _G.get_oil_winbar()
+				local dir = require("oil").get_current_dir()
+				if dir then
+					return vim.fn.fnamemodify(dir, ":~")
+				else
+					return vim.api.nvim_buf_get_name(0)
+				end
+			end
+			local detail = false
+
+			local function parse_output(proc)
+				local result = proc:wait()
+				local ret = {}
+				if result.code == 0 then
+					for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
+						-- Remove trailing slash
+						line = line:gsub("/$", "")
+						ret[line] = true
+					end
+				end
+				return ret
+			end
+
+			local function new_git_status()
+				return setmetatable({}, {
+					__index = function(self, key)
+						local ignore_proc = vim.system(
+							{ "git", "ls-files", "--ignored", "--exclude-standard", "--others", "--directory" },
+							{
+								cwd = key,
+								text = true,
+							}
+						)
+						local tracked_proc = vim.system({ "git", "ls-tree", "HEAD", "--name-only" }, {
+							cwd = key,
+							text = true,
+						})
+						local ret = {
+							ignored = parse_output(ignore_proc),
+							tracked = parse_output(tracked_proc),
+						}
+
+						rawset(self, key, ret)
+						return ret
+					end,
+				})
+			end
+			local git_status = new_git_status()
+
+			local refresh = require("oil.actions").refresh
+			local orig_refresh = refresh.callback
+			refresh.callback = function(...)
+				git_status = new_git_status()
+				orig_refresh(...)
+			end
+
+			return {
+				win_options = {
+					winbar = "%!v:lua.get_oil_winbar()",
+				},
+				delete_to_trash = true,
+				skip_confirm_for_simple_edits = true,
+				keymaps = {
+					["<left>"] = "actions.parent",
+					["<right>"] = "actions.select",
+					["<esc>"] = "actions.close",
+					["?"] = {
+						desc = "Toggle file detail view",
+						callback = function()
+							detail = not detail
+							if detail then
+								require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+							else
+								require("oil").set_columns({ "icon" })
+							end
+						end,
+					},
+				},
+				view_options = {
+					view_options = {
+						is_hidden_file = function(name, bufnr)
+							local dir = require("oil").get_current_dir(bufnr)
+							local is_dotfile = vim.startswith(name, ".") and name ~= ".."
+							if not dir then
+								return is_dotfile
+							end
+							if is_dotfile then
+								return not git_status[dir].tracked[name]
+							else
+								return git_status[dir].ignored[name]
+							end
+						end,
+					},
+					is_always_hidden = function(name)
+						return name == ".."
+					end,
+					sort = {
+						{ "mtime", "desc" },
+					},
+				},
+			}
+		end,
+		keys = {
+			{
+				"<left>",
+				function()
+					local oil = require("oil")
+					oil.open()
+					require("oil.util").run_after_load(0, function()
+						oil.open_preview()
+					end)
+				end,
+				desc = "file manager",
 			},
 		},
 	},
@@ -423,33 +442,6 @@ require("lazy").setup({
 		end,
 	},
 	{
-		"monaqa/dial.nvim",
-		config = function()
-			local create = require("dial.augend").constant.new
-			require("dial.config").augends:register_group({
-				default = {
-					create({ elements = { "let", "const" } }),
-					create({ elements = { "True", "False" } }),
-					create({ elements = { "||", "&&" }, word = false }),
-					create({ elements = { "true", "false" } }),
-					create({ elements = { "and", "or" } }),
-					create({ elements = { "===", "!==" }, word = false }),
-					create({ elements = { "==", "!=" }, word = false }),
-					create({ elements = { "- [ ]", "- [x]" }, word = false }),
-				},
-			})
-		end,
-		keys = {
-			{
-				"<leader>ns",
-				function()
-					require("dial.map").manipulate("increment", "normal")
-				end,
-				desc = "cycle",
-			},
-		},
-	},
-	{
 		"lukas-reineke/indent-blankline.nvim",
 		main = "ibl",
 		opts = {
@@ -479,20 +471,117 @@ require("lazy").setup({
 		},
 	},
 	{
-		"akinsho/toggleterm.nvim",
-		opts = {
-			open_mapping = "<leader>e",
-			insert_mappings = false,
-			terminal_mappings = false,
-			autochdir = true,
-			shade_terminals = false,
-			direction = "tab",
-			shell = vim.fn.has("win32") and 'cmd.exe /s /k "clink inject -q && %userprofile%\\dotfiles\\doskeys.cmd"'
-				or "zsh",
-		},
+		"samjwill/nvim-unception",
 	},
 	{
-		"samjwill/nvim-unception",
+		"chrisgrieser/nvim-various-textobjs",
+		config = function()
+			vim.keymap.set("n", "gx", function()
+				require("various-textobjs").url()
+
+				local foundURL = vim.fn.mode():find("v")
+				if not foundURL then
+					return
+				end
+
+				vim.cmd.normal({ '"zy', bang = true })
+				local url = vim.fn.getreg("z")
+				vim.ui.open(url)
+			end, { desc = "URL Opener" })
+
+			require("various-textobjs").setup({
+				useDefaultKeymaps = false,
+			})
+
+			vim.keymap.set({ "o", "x" }, "ie", "<cmd>lua require('various-textobjs').subword('inner')<cr>")
+			vim.keymap.set({ "o", "x" }, "ae", "<cmd>lua require('various-textobjs').subword('outer')<cr>")
+			vim.keymap.set({ "o", "x" }, "Z", "<cmd>lua require('various-textobjs').entireBuffer()<cr>")
+			vim.keymap.set({ "o", "x" }, "m", "<cmd>lua require('various-textobjs').nearEoL()<cr>")
+			vim.keymap.set({ "o", "x" }, "q", "<cmd>lua require('various-textobjs').lineCharacterwise('inner')<cr>")
+			vim.keymap.set({ "o", "x" }, "iv", "<cmd>lua require('various-textobjs').value('inner')<cr>")
+			vim.keymap.set({ "o", "x" }, "av", "<cmd>lua require('various-textobjs').value('outer')<cr>")
+			vim.keymap.set({ "o", "x" }, "ik", "<cmd>lua require('various-textobjs').key('inner')<cr>")
+			vim.keymap.set({ "o", "x" }, "ak", "<cmd>lua require('various-textobjs').key('outer')<cr>")
+			vim.keymap.set({ "o", "x" }, "u", "<cmd>lua require('various-textobjs').url()<cr>", { desc = "url" })
+			vim.keymap.set({ "o", "x" }, "ig", "<cmd>lua require('various-textobjs').chainMember('inner')<cr>")
+			vim.keymap.set({ "o", "x" }, "ag", "<cmd>lua require('various-textobjs').chainMember('outer')<cr>")
+		end,
+	},
+	{
+		"nikitarevenco/nvim-treesitter-textobjects",
+		branch = "personal",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		opts = {
+			textobjects = {
+				move = {
+					enable = true,
+					goto_next_start = {
+						["]a"] = "@parameter.inner",
+						["]l"] = "@loop.outer",
+						["]m"] = "@function.outer",
+						["]c"] = "@conditional.outer",
+						["]o"] = "@class.outer",
+						["]f"] = "@call.outer",
+						["]h"] = "@block.outer",
+						["]i"] = "@assignment.outer",
+						["]r"] = "@return.outer",
+						["]n"] = "@annotation.outer",
+					},
+					goto_previous_start = {
+						["[a"] = "@parameter.inner",
+						["[l"] = "@loop.outer",
+						["[m"] = "@function.outer",
+						["[c"] = "@conditional.outer",
+						["[o"] = "@class.outer",
+						["[f"] = "@call.outer",
+						["[h"] = "@block.outer",
+						["[i"] = "@assignment.outer",
+						["[r"] = "@return.outer",
+						["[n"] = "@annotation.outer",
+					},
+				},
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["id"] = { query = "@number.inner", desc = "number" },
+						["au"] = { query = "@statement.outer", desc = "statement" },
+						["iu"] = { query = "@assignment.outer", desc = "assignment" },
+            ["ah"] = { query = "@block.outer", desc = "block" },
+            ["ih"] = { query = "@block.inner", desc = "block" },
+            ["ao"] = { query = "@class.outer", desc = "class" },
+            ["io"] = { query = "@class.inner", desc = "class" },
+            ["ai"] = { query = "@assignment.lhs", desc = "assignment lhs" },
+            ["ii"] = { query = "@assignment.rhs", desc = "assignment rhs" },
+            ["in"] = { query = "@annotation.inner", desc = "annotation" },
+            ["an"] = { query = "@annotation.outer", desc = "annotation" },
+            ["af"] = { query = "@call.outer", desc = "function call" },
+            ["if"] = { query = "@call.inner", desc = "function call" },
+            ["ac"] = { query = "@conditional.outer", desc = "conditional" },
+            ["ic"] = { query = "@conditional.inner", desc = "conditional" },
+            ["ir"] = { query = "@return.inner", desc = "return" },
+            ["ar"] = { query = "@return.outer", desc = "return" },
+						["am"] = { query = "@function.outer", desc = "function" },
+						["im"] = { query = "@function.inner", desc = "function" },
+						["al"] = { query = "@loop.outer", desc = "loop" },
+						["il"] = { query = "@loop.inner", desc = "loop" },
+						["aa"] = { query = "@parameter.outer", desc = "argument" },
+						["ia"] = { query = "@parameter.inner", desc = "argument" },
+					},
+				},
+			},
+		},
+		config = function(_, opts)
+			local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+
+			vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+			vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+			vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+			vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+			vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+			vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
+			require("nvim-treesitter.configs").setup(opts)
+		end,
 	},
 }, {
 	checker = {
