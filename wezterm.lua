34466cb882bcde974959f1ec4c6bebf8dd58c749
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-09-16 16:25:03 +0100

    feat(wezterm): leftarrow and right arrow will go to the left and right of word

--- wezterm.lua
+++ wezterm.lua
@@ -37,6 +37,8 @@ config.keys = {
 		mods = "CTRL",
 		action = wezterm.action.ResetFontSize,
 	},
+	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
 	{
 		key = "\\",
 		mods = "CTRL",
