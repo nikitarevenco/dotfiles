85290f2aa9dfe2012680872058ddb392908669b8
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-09-18 18:11:28 +0100

    feat: update i3wm keybindings

--- i3.sh
+++ i3.sh
@@ -1,7 +1,7 @@
-set $mod Mod1
-
 default_border pixel 2
 default_floating_border pixel 2
+# hide borders if only 1 window on workspace
+hide_edge_borders smart
 
 # target                 title        bg        text      indicator   border
 client.focused           #cba6f7      #1e1e2e   #cdd6f4   #f5e0dc     #cba6f7
@@ -18,50 +18,46 @@ bindsym XF86AudioMute exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ to
 bindsym XF86AudioMicMute exec --no-startup-id pactl set-source-mute @DEFAULT_SOURCE@ toggle && $refresh_i3status
 
 # launch programs
-bindsym $mod+s exec wezterm-gui
-bindsym $mod+r exec firefox
-bindsym $mod+d exec --no-startup-id dmenu_run -fn 'JetBrainsMono Nerd Font' -nf "#cdd6f4" -nb "#181825" -sb "#11111b"
+bindsym F7 exec boomer
+bindsym Mod1+s exec wezterm-gui
+bindsym Mod1+r exec firefox
+bindsym Mod1+d exec --no-startup-id dmenu_run -fn 'JetBrainsMono Nerd Font' -nf "#cdd6f4" -nb "#181825" -sb "#11111b"
 
 # kill focused window
-bindsym $mod+t kill
+bindsym Mod1+t kill
 
 # change focus
-bindsym $mod+n focus left
-bindsym $mod+e focus up
-bindsym $mod+i focus down
-bindsym $mod+o focus right
+bindsym Mod1+n focus left
+bindsym Mod1+e focus up
+bindsym Mod1+i focus down
+bindsym Mod1+o focus right
 
 # move focused window
-bindsym $mod+2 move left
-bindsym $mod+4 move down
-bindsym $mod+1 move up
-bindsym $mod+8 move right
+bindsym Mod1+2 move left
+bindsym Mod1+4 move down
+bindsym Mod1+1 move up
+bindsym Mod1+8 move right
 
-bindsym $mod+m fullscreen toggle
-bindsym $mod+g floating toggle
-
-set $ws1 "1"
-set $ws2 "2"
-set $ws3 "3"
-set $ws4 "4"
+bindsym Mod1+m fullscreen toggle
+bindsym Mod1+g floating toggle
 
 # switch to workspace
-bindsym $mod+w workspace number $ws1
-bindsym $mod+f workspace number $ws2
-bindsym $mod+p workspace number $ws3
-bindsym $mod+b workspace number $ws4
+bindsym Mod1+w workspace number 1
+bindsym Mod1+f workspace number 2
+bindsym Mod1+p workspace number 3
+bindsym Mod1+b workspace number 4
 
 # move focused container to workspace
-bindsym $mod+5 move container to workspace number $ws1
-bindsym $mod+0 move container to workspace number $ws2
-bindsym $mod+3 move container to workspace number $ws3
-bindsym $mod+7 move container to workspace number $ws4
+bindsym Mod1+5 move container to workspace number 1
+bindsym Mod1+0 move container to workspace number 2
+bindsym Mod1+3 move container to workspace number 3
+bindsym Mod1+7 move container to workspace number 4
 
 # reload the configuration file
-bindsym $mod+z reload
+bindsym Mod1+z reload
 
 # resize
-bindsym $mod+j resize grow width 10 px or 10 ppt
-bindsym $mod+l resize shrink width 10 px or 10 ppt
-bindsym $mod+u resize grow height 10 px or 10 ppt
-bindsym $mod+y resize shrink height 10 px or 10 ppt
+bindsym Mod1+j resize grow width 10 px or 10 ppt
+bindsym Mod1+l resize shrink width 10 px or 10 ppt
+bindsym Mod1+u resize grow height 10 px or 10 ppt
+bindsym Mod1+y resize shrink height 10 px or 10 ppt
