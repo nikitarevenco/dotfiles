883fd07a6fe4e1aac5625ab71fd05bdb431bf4b0
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-10-12 21:02:20 +0100

    feat(nvim): add keybindings for gitsigns plugin

--- neovim.lua
+++ neovim.lua
@@ -497,7 +497,27 @@ require("lazy").setup({
 	},
 	{
 		"lewis6991/gitsigns.nvim",
-		opts = {},
+		opts = {
+      on_attach = function(bufnr)
+local gitsigns = require('gitsigns')
+    vim.keymap.set('n', ']c', function()
+      if vim.wo.diff then
+        vim.cmd.normal({']c', bang = true})
+      else
+        gitsigns.nav_hunk('next')
+      end
+    end, { buffer = bufnr })
+
+    vim.keymap.set('n', '[c', function()
+      if vim.wo.diff then
+        vim.cmd.normal({'[c', bang = true})
+      else
+        gitsigns.nav_hunk('prev')
+      end
+    end, { buffer = bufnr })
+
+      end
+    },
 	},
 	{
 		"catppuccin/nvim",
