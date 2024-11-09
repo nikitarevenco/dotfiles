a7aad77e26cb92a53bfa66eb7f587dd4892dfaa3
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-11-09 19:09:52 +0000

    feat: use personal fork of helix

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    helix.url = "github:helix-editor/helix/master";
+    helix.url = "github:nikitarevenco/helix/personal";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
